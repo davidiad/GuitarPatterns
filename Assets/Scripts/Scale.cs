37672047052c50b915dc8a5fb2a735e282906fad
David Fierstein <studio@davidiad.com>
2016-09-07 11:47:57 -0700

    Refactor chord generation so that chord menu updates
    In the Scale object, move the scale and chord generation functions, and the chord menu function, to ChangeScale(_rootIndex). Call ChangeScale(_rootIndex) from the Scale menu. The 0 to 11 int sent when value is changed corresponds to the major key. The Chord menu updates to reflect the new key.

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -18,9 +18,18 @@ public class Scale : MonoBehaviour {
 
 	private NoteInfo noteInfo; // to retrieve basic info like the string associated with each note index
 
+
+	private string[] chordPrefix;
+	private string[] sharps;
+	private string[] chordPostfix;
+
 	// Use this for initialization
 	void Awake () {
 		majorScalePattern = new bool[] {true, false, true, false, true, true, false, true, false, true, false, true};
+		chordPrefix = new string[] {"I: ", "ii: ", "iii: ", "IV: ", "V: ", "vi: ", "vii: "};
+//		sharps = new string[] {"", "", "", "", "", "", ""}; // will be blank string unless the note is sharped
+		chordPostfix = new string[] {"", "m", "m", "", "", "m", " dim"};
+
 		scale = new int[7];
 		chords = new Chord[7];
 		chordNames = new string[7];
@@ -28,13 +37,16 @@ public class Scale : MonoBehaviour {
 			chords [i] = new Chord ();
 		}
 
-//		noteNames = new string[12];
+
 		noteInfo = GameObject.FindGameObjectWithTag ("NoteInfo").GetComponent<NoteInfo> ();
 	}
 	
 
 	void Start () {
+
+
 		ScaleMenuSetup ();
+
 		// The pattern for the scale in order by the index of each note
 		// For instance, G Major contains  the 7 notes (G, A, B, C, D, E, F#) so
 		// the pattern by index is (10, 0, 2, 3, 5, 7, 9)
@@ -53,13 +65,56 @@ public class Scale : MonoBehaviour {
 //		GenerateChords ();
 //		ChordMenuSetup ();
 
-		GenerateMajorScale (rootIndex);
+		ChangeScale (rootIndex);
+//		// Generate the Chords for the Scale
+//		for (int i = 0; i < 7; i++) {
+//			// each chord is made up of the 1st root note, followed by one 2 notes above in the scale, followed by 2 notes above that
+//			// circling back to the first one after the 7th, hence the modulus operation
+//			int j = (i + 2) % 7;
+//			int k = (i + 4) % 7;
+//			chords [i].Init (scale [i], scale [j], scale [k]);
+//
+//			if (noteInfo.Sharps[scale[i]]) {
+//				sharps [i] += "#";
+//			}
+//
+//			chords[i].name = chordPrefix[i] + noteInfo.PitchNames [scale [i]] + sharps[i] + chordPostfix[i];
+//			chordNames [i] = chords [i].name;
+//		}
 
+//		ChordMenuSetup ();
+	}
+
+	public void ChangeScale(int _rootIndex) {
+		GenerateMajorScale (_rootIndex);
+		GenerateChords ();
+		ChordMenuSetup ();
+	}
+
+	private void ScaleMenuSetup() {
+		scaleMenu.ClearOptions ();
+		for (int i = 0; i < 12; i++) {
+			string option = noteInfo.NoteRichTexts [i];
+			if (noteInfo.Sharps [i]) {
+				option += "#";
+			}
+			scaleMenu.options.Add (new Dropdown.OptionData (option));
+		}
+	}
+
+	private void ChordMenuSetup() {
+		chordMenu.ClearOptions();
+
+		foreach (string option in chordNames) {
+			chordMenu.options.Add(new Dropdown.OptionData(option));
+		}
+	}
+
+	private void GenerateChords() {
 		// Generate the Chords for the Scale
-		// need to put these into a separate func (GenerateChords()?), so it can be called when changing the scale
-		string[] chordPrefix = {"I: ", "ii: ", "iii: ", "IV: ", "V: ", "vi: ", "vii: "};
-		string[] sharps = {"", "", "", "", "", "", ""}; // will be blank string unless the note is sharped
-		string[] chordPostfix = {"", "m", "m", "", "", "m", " dim"};
+
+		// reset the sharp string each time, so we get only 1 # symbol per note
+		sharps = new string[] {"", "", "", "", "", "", ""}; // will be blank string unless the note is sharped
 
 		for (int i = 0; i < 7; i++) {
 			// each chord is made up of the 1st root note, followed by one 2 notes above in the scale, followed by 2 notes above that
@@ -74,36 +129,7 @@ public class Scale : MonoBehaviour {
 
 			chords[i].name = chordPrefix[i] + noteInfo.PitchNames [scale [i]] + sharps[i] + chordPostfix[i];
 			chordNames [i] = chords [i].name;
-
 		}
-		ChordMenuSetup ();
-	}
-
-	void ScaleMenuSetup() {
-		scaleMenu.ClearOptions ();
-		for (int i = 0; i < 12; i++) {
-			string option = noteInfo.NoteRichTexts [i];
-			if (noteInfo.Sharps [i]) {
-				option += "#";
-			}
-			scaleMenu.options.Add (new Dropdown.OptionData (option));
-		}
-	}
-
-	void ChordMenuSetup() {
-
-		//List<string> list = new List<string> { "G", "Am", "Bm", "D" };
-		chordMenu.ClearOptions();
-
-		foreach (string option in chordNames)
-		{
-			chordMenu.options.Add(new Dropdown.OptionData(option));
-		}
-
-//		foreach (string option in list)
-//		{
-//			chordMenu.options.Add(new Dropdown.OptionData(option));
-//		}
 	}
 
 	private void GenerateMajorScale(int _rootIndex) {
@@ -132,9 +158,5 @@ public class Scale : MonoBehaviour {
 		}
 
 	}
-
-	private void GenerateChords() {
 		
-
-	}
 }
